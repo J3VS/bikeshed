904abd0fd77cc2b9b51829d5b3a487f7df806269
J3VS <jonathan.viccary@gmail.com>
2017-01-16 01:00:10 -0500

    First iteration of image selector

--- bikeshed/static/js/app.js
+++ bikeshed/static/js/app.js
@@ -1,2 +1,20 @@
 $('.bikeImage').height($('.bikeImage').width());
 $('.chosenImage').height($('.chosenImage').width());
+
+function readURL(input) {
+  var file = ($("#id_image"))[0].files[0];
+  if (file) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+          $('#bikeImage')
+              .attr('src', e.target.result)
+              .width($('.chosenImage').width())
+              .height($('.chosenImage').height());
+      };
+
+      reader.readAsDataURL(file);
+  }
+}
+
+$(document).on('change', '#id_image', readURL);
