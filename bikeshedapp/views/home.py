afab62c85f774afe3d102e77a9121e7f841772c8
J3VS <jonathan.viccary@gmail.com>
2017-01-16 00:21:42 -0500

    Styling and list progress

--- bikeshedapp/views/home.py
+++ bikeshedapp/views/home.py
@@ -1,22 +1,7 @@
-from django.shortcuts     import redirect
-from django.views.generic import ListView
-from bikeshedapp.models   import Bike
+from django.views.generic      import ListView
+from django.views.generic.list import MultipleObjectMixin
+from bikeshedapp.models        import Bike
 
-class BikeList(ListView):
-
-    model = Bike;
-
-    def dispatch(self, request, *args, **kwargs):
-        self.request = request     #So get_context_data can access it.
-        return super(BikeList, self).dispatch(request, *args, **kwargs)
-
-    def get_queryset(self):
-        """
-        Returns the all colors, for display in the main table. The search
-        result query set, if any, is passed as context.
-        """
-        return  super(BikeList, self).get_queryset()
-
-    def get_context_data(self, **kwargs):
-        #The current context.
-        context = super(BikeList, self).get_context_data(**kwargs)
+class BikeList(ListView, MultipleObjectMixin):
+    template_name = "bikeshedapp/bike_list.html"
+    model = Bike
